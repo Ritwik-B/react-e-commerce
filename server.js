188d31fbc79d58fbdc64dff37d0023bfd1ac25e3
Ritwik Bhanot <rbhanot999@gmail.com>
2020-11-10 14:11:58 +0530

    updating code to be pwa compliant, added serviceWorker, https, updated manifest

--- server.js
+++ server.js
@@ -12,18 +12,18 @@ const stripe = require("stripe")(process.env.REACT_APP_STRIPE_SECRET_KEY);
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(compression());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(cors());
 
 if (process.env.NODE_ENV === "production") {
+  app.use(compression());
   app.use(enforce.HTTPS({ trustProtoHeader: true }));
   app.use(express.static(path.join(__dirname, "client/build")));
 
   app.get("/service-worker.js", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "..", "build", "service-worker.js"));
+    res.sendFile(path.resolve(__dirname, "client/build", "service-worker.js"));
   });
 
   app.get("*", function (req, res) {
