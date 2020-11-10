80599298e9d195aab6ea8c80fb055b6daeedd503
Ritwik Bhanot <rbhanot999@gmail.com>
2020-11-10 16:26:32 +0530

    fix

--- client/src/index.js
+++ client/src/index.js
@@ -3,9 +3,9 @@ import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
+import * as serviceWorker from "./serviceWorker.js";
 
 import { store, persistor } from "./redux/store";
-import * as serviceWorker from "./serviceWorker";
 
 import App from "./App";
 import "./index.css";
