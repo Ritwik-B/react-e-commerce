cecd2ab24e6cc358d7af7eb3f166b5d3c9b614e0
Ritwik Bhanot <rbhanot999@gmail.com>
2020-11-11 02:05:27 +0530

    fix

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,21 +1,21 @@
-import React from "react";
-import StripeCheckout from "react-stripe-checkout";
-import axios from "axios";
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
 
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
   const publishableKey =
-    "pk_test_51HYs3YG6AC6rBwqT7LdkwVQPfSG7asqs42tb6xP6vtQJu8Ln1f05ji61MoEYkKIrqbeb8CB6Iehmk7CEfHc9dqfL0011J81K1g";
+    'pk_test_51HYs3YG6AC6rBwqT7LdkwVQPfSG7asqs42tb6xP6vtQJu8Ln1f05ji61MoEYkKIrqbeb8CB6Iehmk7CEfHc9dqfL0011J81K1g';
 
   const onToken = (token) => {
     axios({
-      url: "payment",
-      method: "post",
+      url: 'payment',
+      method: 'post',
       data: {
         amount: priceForStripe,
         token: token,
       },
-    }).then(() => alert("Payment Successfull"));
+    });
   };
 
   return (
